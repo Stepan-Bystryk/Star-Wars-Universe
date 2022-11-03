46f88c1c043dfb8ffc0f9385df861ccad74e4212
Stepan <stewie_the.rofl@mail.ru>
2022-11-03 05:16:32 +0300

    props types added

--- src/components/PeoplePage/PeopleList/PeopleList.tsx
+++ src/components/PeoplePage/PeopleList/PeopleList.tsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import styles from "./PeopleList.module.css";
 
 const PeopleList = ({ people }: any) => {
@@ -13,6 +14,10 @@ const PeopleList = ({ people }: any) => {
       ))}
     </ul>
   );
+};
+
+PeopleList.propTypes = {
+  people: PropTypes.array,
 };
 
 export default PeopleList;
