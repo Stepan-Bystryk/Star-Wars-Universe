a9a31c011c3e7a74e74e532af4530230108dfae6
Stepan <stewie_the.rofl@mail.ru>
2022-11-04 21:09:20 +0300

    buttons UI added

---
+++ src/components/UI/UiButton/UiButton.tsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import cn from "classnames";
+
+import "../index.css";
+import styles from "./UiButton.module.css";
+
+const UiButton = ({
+  text,
+  onClick,
+  disabled,
+  theme = "dark",
+  classes,
+}: any) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={cn(styles.button, styles[theme], classes)}
+    >
+      {text}
+    </button>
+  );
+};
+
+UiButton.propTypes = {
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  theme: PropTypes.string,
+  classes: PropTypes.string,
+};
+
+export default UiButton;
